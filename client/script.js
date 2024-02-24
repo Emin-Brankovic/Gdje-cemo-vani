2a63cf030713a57460fc568115d47cab74449b14
Emin Brankovic <emin.brankovic19@gmail.com>
2024-02-24 17:22:33 +0100

    dodata logika za dropdown i promjene izgleda

---
+++ client/script.js
@@ -0,0 +1,5 @@
+const test=(dropdownItem)=>{
+    //let item=document.querySelector(".dropdown-item").textContent;
+    let dugme=document.getElementById("kategorija");
+    dugme.textContent=dropdownItem.textContent;
+}
