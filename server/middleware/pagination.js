d7559db1e88ea460cfb1e399782c29c36419c3c5
Emin Brankovic <emin.brankovic19@gmail.com>
2024-07-03 21:02:10 +0200

    Bug fixed

--- server/middleware/pagination.js
+++ server/middleware/pagination.js
@@ -1,13 +1,14 @@
 import Mjesto from "../model/Mjesto.model.js";
 
 export const paginatedResults=async(req,res,next)=> {
+  
     const page=parseInt(req.query.page);
     const limit=parseInt(req.query.limit);
     const {kategorija,daljina}=req.query;
 
     const startIndex=(page-1)*limit;
     const endIndex=page*limit;
-    const numberOfElements=await Mjesto.countDocuments().exec();
+    let numberOfElements;
 
     const results={};
 
@@ -25,27 +26,34 @@ export const paginatedResults=async(req,res,next)=> {
         }
       }
 
-      results.lastPage=Math.ceil(numberOfElements/limit);
       results.currentPage=page;
 
       try {
         if(kategorija.length!==0 && daljina.length!==0){
             results.results = await Mjesto.find({ kategorija: kategorija, daljina: daljina}).limit(limit).skip(startIndex).exec()
+            numberOfElements=await Mjesto.countDocuments({ kategorija: kategorija, daljina: daljina}).exec();
+            results.lastPage=Math.ceil(numberOfElements/limit);
             res.paginatedResults = results
             next()
         }
         else if(kategorija.length!==0 && daljina.length===0){
             results.results = await Mjesto.find({ kategorija: kategorija}).limit(limit).skip(startIndex).exec()
+            numberOfElements=await Mjesto.countDocuments({ kategorija: kategorija}).exec();
+            results.lastPage=Math.ceil(numberOfElements/limit);
             res.paginatedResults = results
             next()
         }
         else if(kategorija.length===0 && daljina.length!==0){
             results.results = await Mjesto.find({ daljina: daljina}).limit(limit).skip(startIndex).exec()
+            numberOfElements=await Mjesto.countDocuments({ daljina: daljina}).exec();
+            results.lastPage=Math.ceil(numberOfElements/limit);
             res.paginatedResults = results
             next()
         }
         else{
             results.results = await Mjesto.find().limit(limit).skip(startIndex).exec()
+            numberOfElements=await Mjesto.countDocuments().exec();
+            results.lastPage=Math.ceil(numberOfElements/limit);
             res.paginatedResults = results
             next()
         }
